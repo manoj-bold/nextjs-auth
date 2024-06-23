4bd66c8399fde1484ee06dcb2149675a1c827417
Manoj Mishra <manoj.mishra@bold.com>
2024-06-24 01:19:50 +0530

    Add server side page guard

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,25 +1,25 @@
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  const { data: session, status } = useSession();
-  const loading = status === 'loading';
+  // const { data: session, status } = useSession();
+  // const loading = status === 'loading';
 
-  if (loading) {
-    return <p className={classes.profile}>Loading...</p>;
-  } else {
-    if (!session) {
-      window.location.href = '/auth';
-    } else {
+  // if (loading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // } else {
+  //   if (!session) {
+  //     window.location.href = '/auth';
+  //   } else {
       return (
         <section className={classes.profile}>
           <h1>Your User Profile</h1>
           <ProfileForm />
         </section>
       );
-    }
-  }
+  //   }
+  // }
 }
 
 export default UserProfile;
--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,25 @@
+import { getSession } from 'next-auth/react';
 import AuthForm from '../components/auth/auth-form';
 
 function AuthPage() {
   return <AuthForm />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+  
+  if (session) {
+    return {
+      redirect: {
+        destination: '/profile',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
 
 export default AuthPage;
