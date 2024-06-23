37176873057f2f5bfa6f6a15d4533510c34506dc
Manoj Mishra <manoj.mishra@bold.com>
2024-06-24 00:50:57 +0530

    Add client side page guard

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,15 +1,25 @@
+import { useSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const { data: session, status } = useSession();
+  const loading = status === 'loading';
 
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+  if (loading) {
+    return <p className={classes.profile}>Loading...</p>;
+  } else {
+    if (!session) {
+      window.location.href = '/auth';
+    } else {
+      return (
+        <section className={classes.profile}>
+          <h1>Your User Profile</h1>
+          <ProfileForm />
+        </section>
+      );
+    }
+  }
 }
 
 export default UserProfile;
